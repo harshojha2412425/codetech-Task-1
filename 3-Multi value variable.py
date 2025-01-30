41877f836fa18bc285a80e1f51a3ac1a15d636a5
harshojha2412425 <harshojha2412@gmail.com>
2025-01-30 12:11:21 +0530

    Add files via upload

---
+++ 3-Multi value variable.py
@@ -0,0 +1,25 @@
+#Multi value
+'''x,y,z = "orange","Banana","Apple"
+print(x)
+print(y)
+print(z)
+
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+Fruits = ["orange","Banana","apple"]
+x,y,z = Fruits 
+print(x)
+print(y)
+print(z)
+
+# output variable
+x = "my name is harsh"
+print(x) '''
+
+x = 5 
+y = 4
+z = 2
+print(x +  y + z)
---
+++ 5- Data type.py
@@ -0,0 +1,9 @@
+x = 1
+y = 2.5
+z = 1j
+print(type(x))
+print(type(y))
+print(type(z))
+
+import random
+print(random.randrange(1,10))
